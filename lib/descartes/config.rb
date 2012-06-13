d9ef78e40e576d309042fa690fa3496155414930
obfuscurity <jdixon@heroku.com>
2012-06-13 12:52:24 -0400

    import config lib

---
+++ lib/descartes/config.rb
@@ -0,0 +1,15 @@
+module Descartes
+  module Config
+    def self.env!(key)
+      ENV[key] || raise("missing #{key}")
+    end
+
+    def self.canonical_host; ENV["CANONICAL_HOST"]; end
+    def self.force_https; ENV["FORCE_HTTPS"]; end
+    def self.google_oauth_domain; env!("GOOGLE_OAUTH_DOMAIN"); end
+    def self.port; env!("PORT").to_i; end
+    def self.rack_env; env!("RACK_ENV"); end
+    def self.user; env!("REMOTE_USER"); end
+    def self.session_secret; env!("SESSION_SECRET"); end
+  end
+end
