7f5be1ba9a59db21061475a5d678102fd2b140bc
obfuscurity <jdixon@heroku.com>
2012-07-02 13:56:23 -0400

    form to create new dashboard from current graph/filter view

--- lib/descartes/routes/dashboards.rb
+++ lib/descartes/routes/dashboards.rb
@@ -2,12 +2,35 @@ module Descartes
   class Web < Sinatra::Base
 
     get '/dashboards/?' do
+      if params[:owner]
+        @dashboards = Dashboard.filter(:enabled => true, :owner => session['user']['email']).all
+      else
+        @dashboards = Dashboard.filter(:enabled => true).all
+      end
+      haml :dashboards, :locals => { :dashboards => @dashboards }
     end
 
     post '/dashboards/?' do
+      if params[:graphs] && params[:name]
+        graphs = []
+        graphs.push(params[:graphs].split(",")).flatten!
+        configuration = { :graphs => graphs }
+        @dashboard = Dashboard.new({ :owner => session['user']['email'], :name => params[:name], :configuration => configuration.to_json })
+        @dashboard.save
+        @dashboard.to_json
+      else
+        #halt
+      end
     end
 
     get '/dashboards/:id/?' do
+      #if request.accept.include?("application/json")
+      #  @dashboard = Dashboard.filter(:enabled => true, :uuid => params[:id])
+      #  content_type "application/json"
+      #  @dashboard.to_json
+      #else
+      #  haml :dashboards
+      #end
     end
 
     put '/dashboards/:id/?' do
