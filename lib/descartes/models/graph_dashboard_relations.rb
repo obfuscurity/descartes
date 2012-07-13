df185e21a781ca73a65b0cbb4a881ecbaee7aa96
obfuscurity <jdixon@heroku.com>
2012-07-12 17:10:43 -0700

    delete dashboard if no more graphs remain

--- lib/descartes/models/graph_dashboard_relations.rb
+++ lib/descartes/models/graph_dashboard_relations.rb
@@ -12,4 +12,9 @@ class GraphDashboardRelation < Sequel::Model
   many_to_one :graphs
   many_to_one :dashboards
 
+  def after_destroy
+    if GraphDashboardRelation.filter(:dashboard_id => self.dashboard_id).empty?
+      Dashboard[self.dashboard_id].destroy
+    end
+  end
 end
