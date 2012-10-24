e79bb9cc40c0d64431e3f2c75c355ba31502f6fa
obfuscurity <jason@dixongroup.net>
2012-10-24 17:57:32 -0400

    allow gists to have names

--- lib/descartes/migrations/003_AddGraphGists.rb
+++ lib/descartes/migrations/003_AddGraphGists.rb
@@ -6,6 +6,7 @@ Sequel.migration do
       String      :uuid,       :size => 32,  :null => false, :index => true, :unique => true
       String      :owner,      :size => 80,  :null => false, :index => true
       String      :image,                    :null => false, :index => true
+      String      :name,       :size => 80
       Text        :description
       DateTime    :created_at,               :null => false
       DateTime    :expires_at
