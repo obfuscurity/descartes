4227863c451afb4293035db44f287e7f922680f1
obfuscurity <jason@dixongroup.net>
2012-10-24 23:04:08 -0400

    add g_uuid column for graphs/gists

--- lib/descartes/migrations/004_AddComments.rb
+++ lib/descartes/migrations/004_AddComments.rb
@@ -3,6 +3,7 @@ Sequel.migration do
   up do
     create_table(:comments) do
       primary_key :id
+      String      :g_uuid,     :size => 32,  :null => false, :index => true, :unique => true
       String      :uuid,       :size => 32,  :null => false, :index => true, :unique => true
       String      :owner,      :size => 80,  :null => false, :index => true
       Text        :body
