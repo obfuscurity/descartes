d0dfa88753da4b8427ac6d38888ed922e7c1c8ae
obfuscurity <jason@dixongroup.net>
2012-10-24 20:49:13 -0400

    migration for comments

---
+++ lib/descartes/migrations/004_AddComments.rb
@@ -0,0 +1,17 @@
+
+Sequel.migration do
+  up do
+    create_table(:comments) do
+      primary_key :id
+      String      :uuid,       :size => 32,  :null => false, :index => true, :unique => true
+      String      :owner,      :size => 80,  :null => false, :index => true
+      Text        :body
+      DateTime    :created_at,               :null => false
+    end
+  end
+
+  down do
+    drop_table(:comments)
+  end
+end
+
