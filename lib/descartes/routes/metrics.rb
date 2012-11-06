55ecad2b45d0d0c6f13fa0e6636df15335e958ba
obfuscurity <jason@dixongroup.net>
2012-11-06 16:35:38 -0500

    new metrics search endpoint

--- lib/descartes/routes/metrics.rb
+++ lib/descartes/routes/metrics.rb
@@ -5,5 +5,15 @@ module Descartes
       haml :'metrics/list', :title => "Descartes - Metrics List"
     end
 
+    get '/metrics/search/?' do
+      if request.accept.include?("application/json")
+        content_type "application/json"
+        status 200
+        Metric.find(params[:pattern]).to_json
+      else
+        # halt
+      end
+    end
+
   end
 end
