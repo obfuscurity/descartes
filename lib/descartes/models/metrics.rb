caec3f27f61518c310652c003f777acdb808097b
obfuscurity <jason@dixongroup.net>
2012-11-06 16:33:24 -0500

    new model to cache our metrics list

---
+++ lib/descartes/models/metrics.rb
@@ -0,0 +1,24 @@
+class Metric
+  @@paths = []
+
+  def self.all
+    @@paths
+  end
+
+  def self.find(search)
+    results = []
+    @@paths.each do |p|
+      results << p if p.match(/#{search}/i)
+    end
+   results 
+  end
+
+  def self.load
+    self.update
+  end
+
+  def self.update
+    response = RestClient.get("#{ENV['GRAPHITE_URL']}/metrics/index.json")
+    @@paths = JSON.parse(response)
+  end
+end
