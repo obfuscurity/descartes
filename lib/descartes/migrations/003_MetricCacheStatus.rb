1db33017aeb26d27ea1718326007a549b1059e7f
obfuscurity <jason@dixongroup.net>
2013-03-11 20:32:24 -0400

    try again

--- lib/descartes/migrations/003_MetricCacheStatus.rb
+++ lib/descartes/migrations/003_MetricCacheStatus.rb
@@ -4,7 +4,7 @@ Sequel.migration do
     create_table(:metric_cache_status) do
       DateTime :updated_at, :null => false
     end
-    self[:metric_cache_status].insert(:updated_at, Sequel.function(:NOW))
+    self[:metric_cache_status].insert(Sequel.function(:NOW))
   end
 
   down do
