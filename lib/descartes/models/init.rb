a0ca97d3d48f6cb57c0c5a8b121602b7800b62f9
obfuscurity <jason@dixongroup.net>
2013-05-06 18:17:44 -0400

    need to call Sequel via Unicorn after_fork

--- lib/descartes/models/init.rb
+++ lib/descartes/models/init.rb
@@ -2,9 +2,6 @@ require 'sequel'
 require 'securerandom'
 require 'json'
 
-db = ENV['DATABASE_URL'] || 'postgres://localhost/descartes'
-Sequel.connect(db)
-
 $LOAD_PATH.unshift File.dirname(__FILE__)
 require 'graphs'
 require 'tags'
