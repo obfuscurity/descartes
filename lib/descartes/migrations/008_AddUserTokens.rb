83a13b8ab087c139af61c1b4c0e9d6f45c0232d8
obfuscurity <jason@dixongroup.net>
2013-05-12 00:54:57 -0400

    wrong string len, refs #87

--- lib/descartes/migrations/008_AddUserTokens.rb
+++ lib/descartes/migrations/008_AddUserTokens.rb
@@ -2,7 +2,7 @@
 Sequel.migration do
   up do
     alter_table(:users) do
-      add_column :token, String, :size => 32, :null => false, :index => true, :unique => true
+      add_column :token, String, :size => 64, :null => false, :index => true, :unique => true
     end
   end
 
