0358d9411b17e1c134c30e26a1b4b7084beefe23
obfuscurity <jason@dixongroup.net>
2013-05-13 15:20:01 -0400

    pass back empty array instead of 204

--- lib/descartes/routes/chartroulette.rb
+++ lib/descartes/routes/chartroulette.rb
@@ -5,12 +5,8 @@ module Descartes
       if request.accept.include?("application/json")
         favorites = User.filter(:email => session['user']['email']).first.favorites
         content_type 'application/json'
-        if !favorites.empty?
-          status 200
-          Dashboard.filter(:enabled => true).where(:uuid => favorites).to_json
-        else
-          status 204
-        end
+        status 200
+        Dashboard.filter(:enabled => true).where(:uuid => favorites).to_json
       else
         haml :chartroulette, :locals => { :title => 'Descartes - Chartroulette' }
       end
