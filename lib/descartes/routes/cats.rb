5d5fb871e293484e895d82d99339e51f3e5ad90f
obfuscurity <jason@dixongroup.net>
2013-05-14 16:08:53 -0400

    route for proxing cat images

---
+++ lib/descartes/routes/cats.rb
@@ -0,0 +1,24 @@
+module Descartes
+  class Web < Sinatra::Base
+
+    get '/cats/:count/?' do
+      if request.accept.include?('application/json')
+        content_type 'application/json'
+        @cats = []
+        begin
+          doc = Nokogiri::HTML(open("http://thecatapi.com/api/images/get?format=xml&results_per_page=#{params[:count]}"))
+          doc.css('url').each do |kitty|
+            @cats << kitty.content
+          end
+        rescue => e
+          p e.message
+          halt
+        end
+        status 200
+        @cats.to_json
+      else
+        halt 404
+      end
+    end
+  end
+end
