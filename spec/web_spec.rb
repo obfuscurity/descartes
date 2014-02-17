4a87798b2526920c2357a61074770202ecf1d0aa
Jeff Forcier <jeff@bitprophet.org>
2014-02-17 15:46:11 -0800

    Some skeleton tests

--- spec/web_spec.rb
+++ spec/web_spec.rb
@@ -2,6 +2,8 @@ require 'rack/test'
 require 'spec_helper'
 
 require './lib/descartes/web'
+require './lib/descartes/models/metrics'
+
 
 describe Descartes::Web do
   include Rack::Test::Methods
@@ -10,5 +12,14 @@ describe Descartes::Web do
     Descartes::Web
   end
 
-  it 'should have some tests'
+  it 'should have more tests'
+
+  describe '/metrics/' do
+    context 'json' do
+      it 'loads entire cache without any params'
+      it 'loads specific pages when requested via :page'
+      it 'changes page size optionally via :limit'
+      it 'defaults to page size of 50 metrics'
+    end
+  end
 end
